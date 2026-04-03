547aec1385a91690ffdbfa695359816ee1037bce
Adam Cavillon <adam@codinbox.fr>
2026-04-03 14:50:09 -0400

    feat: add healthcheck configuration environment variables

--- api/src/main/java/fr/codinbox/echo/api/utils/EnvUtils.java
+++ api/src/main/java/fr/codinbox/echo/api/utils/EnvUtils.java
@@ -11,6 +11,15 @@ public class EnvUtils {
     public static final @NotNull String ENV_RESOURCE_ID = "ECHO_RESOURCE_ID";
     public static final @NotNull String ENV_RESOURCE_ADDRESS = "ECHO_RESOURCE_ADDRESS";
 
+    public static final @NotNull String ENV_HEARTBEAT_TTL = "ECHO_HEARTBEAT_TTL";
+    public static final @NotNull String ENV_HEARTBEAT_INTERVAL = "ECHO_HEARTBEAT_INTERVAL";
+    public static final @NotNull String ENV_SCAN_INTERVAL = "ECHO_SCAN_INTERVAL";
+    public static final @NotNull String ENV_HEALTHCHECK_CLEANUP_ENABLED = "ECHO_HEALTHCHECK_CLEANUP_ENABLED";
+
+    private static final long DEFAULT_HEARTBEAT_TTL = 30;
+    private static final long DEFAULT_HEARTBEAT_INTERVAL = 10;
+    private static final long DEFAULT_SCAN_INTERVAL = 15;
+
     public static @Nullable EchoResourceType getResourceType() {
         final String resourceType = System.getenv(ENV_RESOURCE_TYPE);
 
@@ -29,6 +38,29 @@ public class EnvUtils {
         if (address == null)
             return null;
         return Address.fromString(address);
+    }
+
+    public static long getHeartbeatTtl() {
+        final String value = System.getenv(ENV_HEARTBEAT_TTL);
+        if (value == null) return DEFAULT_HEARTBEAT_TTL;
+        return Long.parseLong(value);
+    }
+
+    public static long getHeartbeatInterval() {
+        final String value = System.getenv(ENV_HEARTBEAT_INTERVAL);
+        if (value == null) return DEFAULT_HEARTBEAT_INTERVAL;
+        return Long.parseLong(value);
+    }
+
+    public static long getScanInterval() {
+        final String value = System.getenv(ENV_SCAN_INTERVAL);
+        if (value == null) return DEFAULT_SCAN_INTERVAL;
+        return Long.parseLong(value);
+    }
+
+    public static boolean isHealthcheckCleanupEnabled() {
+        final String value = System.getenv(ENV_HEALTHCHECK_CLEANUP_ENABLED);
+        return Boolean.parseBoolean(value);
     }
 
 }
